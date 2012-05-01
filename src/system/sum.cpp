fed8ada0b17000703219a5061ed9f09b8e310bbd
corset <josunedesosakerejeta@gmail.com>
2012-05-01 12:48:07 +0300

    Code modification, change variable name

--- src/system/sum.cpp
+++ src/system/sum.cpp
@@ -1,4 +1,9 @@
-#include <stdio.h> 
+#include <stdio.h>
+int sum (int num1, int num2)
+{ return num1+num2;
+}
+int main(int num1, int num2) { int x; x=sum(num1,num2); printf("%d\n", x); return 0;
+}#include <stdio.h> 
  
 int sum (int num1, int num2)
 {
