ac264e63a336e7022665a97241ff4e4bf76b566c
corset <josunedesosakerejeta@gmail.com>
2012-09-08 12:25:00 +0300

    Update src/system/sum.cpp

--- src/system/sum.cpp
+++ src/system/sum.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
-int sum (int num2, int num1)
-{ return num2+num1;
+int sum (int num0, int num1)
+{ return num0+num1;
 }
-int main(int num2, int num1)
-{ int x; x=sum(num2,num1); printf("%d\n", x); return 0;
+int main(int num0, int num1)
+{ int x; x=sum(num0,num1); printf("%d\n", x); return 0;
 }
